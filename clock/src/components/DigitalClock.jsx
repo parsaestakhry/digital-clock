aa30acc8e494fdceed3d7202690bc3e50a784af8
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-02-10 15:31:01 +0330

    implemented css

--- clock/src/components/DigitalClock.jsx
+++ clock/src/components/DigitalClock.jsx
@@ -1,3 +1,11 @@
 export const DigitalClock = () => {
-    
+    return (
+        <>
+        <div className="clock-container">
+            <div className="clock">
+                <span>00:00:00</span>
+            </div>
+        </div>
+        </>
+    )
 }
