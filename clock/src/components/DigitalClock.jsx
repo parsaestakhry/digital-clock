c0332311341a8ed415171e582b20b1549f208afc
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-02-10 15:42:59 +0330

    code reformat

--- clock/src/components/DigitalClock.jsx
+++ clock/src/components/DigitalClock.jsx
@@ -1,36 +1,41 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export const DigitalClock = () => {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
 
-    const[time, setTime] = useState(new Date())
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setTime(new Date())
-        }, 1000)
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
-        return () => {
-            clearInterval(intervalId)
-        }
-    }, [])
+  const padZero = (number) => {
+    return (number < 10 ? "0" : "") + number;
+  };
 
-    const formatTime = () => {
-        let hours = time.getHours()
-        const minutes = time.getMinutes()
-        const seconds = time.getSeconds()
-        const meridiem = hours >= 12 ? "PM" : "AM"
+  const formatTime = () => {
+    let hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const meridiem = hours >= 12 ? "PM" : "AM";
 
-        hours = hours % 12 || 12
+    hours = hours % 12 || 12;
 
-        return `${hours} : ${minutes} : ${seconds} ${meridiem}`
-    }
+    return `${padZero(hours)} : ${padZero(minutes)} : ${padZero(
+      seconds
+    )} ${meridiem}`;
+  };
 
-    return (
-        <>
-        <div className="clock-container">
-            <div className="clock">
-                <span>{formatTime()}</span>
-            </div>
+  return (
+    <>
+      <div className="clock-container">
+        <div className="clock">
+          <span>{formatTime()}</span>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
